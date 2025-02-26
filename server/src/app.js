85163b363ee35937818f90f75fc7a6c678ea622e
shireen <49595391+ShireenGadru@users.noreply.github.com>
2025-02-26 21:28:18 +0530

    make new branch for dev

--- server/src/app.js
+++ server/src/app.js
@@ -2,13 +2,8 @@ import express from "express";
 import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
 import chatRouter from "./routes/chatRouter.js";
-import path from "path";
-import url, { fileURLToPath } from "url";
 
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
@@ -20,12 +15,5 @@ app.use(
 app.use(express.json());
 app.use(clerkMiddleware());
 app.use("/", chatRouter);
-
-// PRODUCTION
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-});
 
 export { app };
